e7b446c5e316c64874c7ed14133d1a16d4d5267a
Novaras <tfear091@gmail.com>
2021-12-12 08:06:26 +0000

    wip; ui for building select & place

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-	import "";
+	import { selected } from "./selection";
 	import MainCanvas from "./MainCanvas.svelte";
+	import ControlPanel from "./ControlPanel.svelte";
 
 	import type { LevelMap } from "./map";
+	import type { Building } from "./buildings";
 
 	export let map: LevelMap;
 
 	console.log(map);
+
+	let cur_selection: Building | undefined;
+	selected.subscribe((value: Building) => {
+		cur_selection = value;
+	});
 </script>
 
 <main id="app_main">
 	<div class="mb-4">Pharaoh!</div>
+	<div>
+		selected: <pre>{cur_selection?.label}</pre>
+	</div>
 	<MainCanvas {map} />
+	<ControlPanel />
 </main>
 
 <style global lang="postcss">
--- src/MainCanvas.svelte
+++ src/MainCanvas.svelte
@@ -8,24 +8,24 @@
 
 	let canvas: HTMLCanvasElement;
 
+	const tile_size = 512 / map.H; // in px
+
 	onMount(() => {
 		const ctx = canvas.getContext("2d")!;
 		const clearCanvas = canvasUtil.clearAllFn(ctx);
-
-		const tile_size = 512 / map.H; // in px
 
 		const render: FrameRequestCallback = (time: number) => {
 			clearCanvas();
 
 			for (const [index, tile] of map.tiles.entries()) {
-				// x = n % H;	x' = Sx
-				// y = floor(n / H);	y' = Sy
 				if (tile.id === 0) {
 					ctx.fillStyle = `lightblue`;
 				} else {
 					ctx.fillStyle = `gold`;
 				}
 
+				// x = n % H;	x' = Sx
+				// y = floor(n / H);	y' = Sy
 				ctx.fillRect(
 					tile_size * (index % map.H),
 					tile_size * Math.floor(index / map.H),
@@ -41,14 +41,32 @@
 				);
 			}
 
-			// requestAnimationFrame(render);
+			setTimeout(() => requestAnimationFrame(render), 50);
 		};
 
 		let frame = requestAnimationFrame(render);
 	});
+
+	const foo = (ev: MouseEvent) => {
+		console.log(ev);
+
+		const x = Math.floor(ev.clientX / tile_size);
+		const y = Math.floor((ev.clientY - 50) / tile_size);
+		const tile = map.tiles[Math.floor(x + map.H * y)];
+
+		console.log(x, y);
+		console.log(Math.floor(x + map.H * y));
+		console.log(tile);
+	};
 </script>
 
-<canvas id="main_canvas" bind:this={canvas} width="512" height="512">
+<canvas
+	id="main_canvas"
+	bind:this={canvas}
+	width="512"
+	height="512"
+	on:click={foo}
+>
 	Need canvas support to play Pharaoh!
 </canvas>
 
